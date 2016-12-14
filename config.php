c4d7b426766579321694b0ca9a1a000298da52f8
Ardi Ragil Saputra <ardi.rs@gmail.com>
2016-12-14 22:56:36 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+$config = [
+            'cilent_id'     =>'japanilasugoi',
+            'cilent_secret' =>'japanilayamette',
+            'redirect_url'  =>'japanila.github.io/index.php',
+            'scope'         =>'user',
+            'app_name'      =>'data',
+            'auth_url'      =>'https://github.io',
+            'api_url'       =>'https://api.github',
+];
+?>
